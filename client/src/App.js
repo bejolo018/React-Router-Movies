9fbed4a9b7f34d3fb1c91152f057bef4b441dfc7
unknown <bejolo018@gmail.com>
2019-05-14 15:48:06 -0500

    can switch between movie links and home

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,9 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path='/' component={MovieList} />
-          <Route exact path='/movies/:id' component={Movie}/>
+          <Route exact path='/movies/:id' render={ (props) => {
+            return (<Movie {...props} addToSavedList={this.addToSavedList}/>)
+          }}/>
 
         </div>
       </div>
